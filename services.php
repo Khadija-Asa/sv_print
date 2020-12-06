a9df8955434461dfd3c3c0255f4e39bce1352c42
khadija.aitsiali <khadija.aitsiali@gmail.com>
2020-12-06 19:45:08 +0100

    add images, renamed files and services page done

---
+++ services.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="services.css">
+  <link rel="stylesheet" href="css/header.css">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Red+Rose:wght@300;400;500;600;700&display=swap" rel="stylesheet"> 
+  <title>Services</title>
+</head>
+
+  <body>
+  
+    <div class="title">
+      <h1>Nos services</h1>
+      <span class="horizontal"></span>
+    </div>
+
+    <div class="part-1">
+      <div class="image-1">
+        <img src="/images/hand_shake.jpg" alt="Se serre la main">
+      </div>
+      <div class="paragraph-1">
+        <p>
+          Les <span class="highlight">meilleurs</span> tarifs pour les <span class="highlight">meilleurs</span> produits !
+          <br>
+          <br>
+          <span class="highlight">Experts</span> du marché des imprimantes.
+          <br>
+          <span class="highlight">SV Print</span> répond à vos attentes selon vos habitudes, vos besoin, votre projet, et votre budget.
+        </p>
+      </div>
+    </div>
+
+    <div class="part-2">
+      <div class="image-2">
+        <img src="/images/time 2.png" alt="Horloge">
+      </div>
+      <div class="paragraph-2">
+        <p>
+          Avec nous tout va <span class="highlight">plus vite</span> !
+          <br>
+          <br>
+          En plus, la livraison est <span class="highlight">gratuite</span> et vous ne paierez votre commande qu’à réception!   
+        </p>
+      </div>
+    </div>
+
+    <div class="part-3">
+      <div class="paragraph-3">
+        <p>
+          Notre équipe se fera un plaisir de vous <span class="highlight">accompagner</span> personnellement pour vous aider au mieux dans vos démarches.
+          <br>
+          <br>
+          Nous vous <span class="highlight">conseillerons</span> sur le <span class="highlight">set-up</span> de votre imprimante et où acheter votre imprimante.
+        </p>
+      </div>
+      <div class="image-3">
+        <img src="/images/panne.jpg" alt="Panne">
+      </div>
+    </div>
+
+    <div class="title">
+      <h1>Écologie</h1>
+      <span class="horizontal-2"></span>
+    </div>
+
+    <div class="part-4">
+      <div class="image-4">
+        <img src="/images/recyclage-nobg.png" alt="Bac recyclage">
+      </div>
+      <div class="paragraph-4">
+        <p>
+          Notre équipe se fera un plaisir de vous <span class="highlight">accompagner</span> personnellement pour vous aider au mieux dans vos démarches.
+          <br>
+          <br>
+          Nous vous <span class="highlight">conseillerons</span> sur le <span class="highlight">set-up</span> de votre imprimante et où acheter votre imprimante.
+        </p>
+      </div>
+    </div>
+    
+  </body>
+</html> 
